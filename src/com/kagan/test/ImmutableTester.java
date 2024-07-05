12d31bca8c59b33195a165b6a6fdda9a456c7a77
sismane <sismane@hotmail.com>
2024-07-05 15:59:47 +0300

    comment changed

--- src/com/kagan/test/ImmutableTester.java
+++ src/com/kagan/test/ImmutableTester.java
@@ -4,7 +4,7 @@ public class ImmutableTester {
 
 	public static void main(String[] args) {
 		Immutable1 immutable1 = new Immutable1(1);
-		//immutable1.count = 2;//compile error because final field can not be assigned!
+		//immutable1.count = 2;//compile error because final field can not be assigned after first initialization!
         Immutable2 immutable2 = new Immutable2();
         ImmutableBase base = new ImmutableBase();//final base class
 	}
